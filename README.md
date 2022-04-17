6c2aef6cb62a5094cfd66dc5280d9d3a15c99ce4
Kostadin Draganov <kostadindraganov@gmail.com>
2022-04-17 03:04:37 +0300

    Added footer bottom menu

--- README.md
+++ README.md
@@ -1,9 +1,11 @@
 
 # Building-of-the-Year
 
+Design link: https://xd.adobe.com/view/0e3cb5b0-0c65-469e-b326-363d1dee909f-5b6f/
+
 ## Quickstart
 
-- install node.js
+- Install node.js
 - Use vsCode
 - Install Live Server vcCode plugin
 
--- tailwind.config.js
+++ tailwind.config.js
@@ -14,17 +14,40 @@ module.exports = {
         regularCyrillic: ['Neue-regular-cyrillic', ...defaultTheme.fontFamily.sans],
       },
       fontSize: {
-        'xs': '.75rem',  // 12px
-        'sm': '.938rem', // 15px
-        'lg': '1.25rem', // 20px
-        'xl': '2.5rem',  // 40px
-        '2xl': '3.5rem', // 56px
+        'uxs': '.625rem', // 1opx
+        'xs': '.688rem', // 11px
+        'sm': '.75rem',  // 12px
+        'lg': '.813rem', // 13px
+        'base': '.875rem', // 14px
+        'xl': '.938rem',  // 15px
+        '2xl': '1.25rem', // 20px
+        '3xl': '2.5rem', // 40px
+        '4xl': '3.5rem', // 56px
       },
       width: {
-        "270px": "270px",
+        '3.8': '0.938rem',// 15px
+        '4.5': '1.125rem',// 18px
+        '23': '5.625rem', // 90px
+        '26': '6.625rem', // 106px
+        '42': '10.25rem', // 164px
+        '71': '17.75rem', // 284px
+        '78': '18.75rem', // 300px
+        '155': '37rem',   // 592px
+      },
+
+      margin: {
+        '7.1': '1.875rem', // 30px
+        '9.5': '2.4rem', // 38px
+        '18': '4.375rem',  // 70px
+        '27': '6.75rem'   // 108
+      },
+      borderWidth: {
+        '1': '1px',
+        '10': '10px'
       },
       backgroundImage: {
-        'splash-bg': "url('/src/assets/images/splash/splash-bg.jpg')",
+        'splash': "url('/src/assets/images/backgrounds/splash-bg.jpg')",
+        'footer': "url('/src/assets/images/backgrounds/footer-bg.jpg')"
       }
     },
     container: {
